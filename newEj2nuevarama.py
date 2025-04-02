4f13b873210bb714686252dae51f8788364e8b3f
Florencia <florenciaanabelintaschi@gmail.com>
2025-04-02 14:21:00 -0300

    agregando file a nueva rama

---
+++ newEj2nuevarama.py
@@ -0,0 +1,5 @@
+print ("Hola, vamos a aprender las funciones basicas de Python.")
+nombre = input ("Como te llamas?")
+print ("Mucho gusto, " + nombre + "!")
+materia = "Programción I"
+print ("¡Wow, " + nombre + " ya estás lista para programar! Sos alumna de" + materia )
